7375034724225ab9c9a24446c5fc4f27f4680021
adamkudz <adam.kudzin@gmail.com>
2022-10-18 13:41:10 -0400

    upload dist

---
+++ dist/cautionMessages.js
@@ -0,0 +1,224 @@
+export const cautionMessages = [
+  {
+    id: 0,
+    message: "CARGO DOOR",
+    description: "Forward cargo door open",
+    messageType: "caution"
+  },
+  {
+    id: 1,
+    message: "CHIP",
+    description: "Oil chip detector on (if installed)",
+    messageType: "caution"
+  },
+  {
+    id: 2,
+    message: "ESP DEGRADED -AOA",
+    description:
+      "Electronic Stability and Protection (ESP?ll) angle of attack (AOA) mode is inoperative.  The system should be serviced",
+    messageType: "caution"
+  },
+  {
+    id: 3,
+    message: "ESP DEGRADED - IAS",
+    description:
+      "Electronic Stability and Protection (ESP?ll) indicated air speed (IAS) mode is inoperative.  The system should be serviced",
+    messageType: "caution"
+  },
+  {
+    id: 4,
+    message: "ESP FAIL",
+    description:
+      "The ESP function has failed and is inoperative. The system should be serviced",
+    messageType: "caution"
+  },
+  {
+    id: 5,
+    message: "FUEL IMIBALANCE",
+    description: "Fuel tanks imbalanced by more than 15 USGAL for >30 seconds",
+    messageType: "caution"
+  },
+  {
+    id: 6,
+    message: "FUEL LOW L-R*",
+    description:
+      "Fuel quantity less than or equal to 9.1 USGAL in specified tank",
+    messageType: "caution"
+  },
+  {
+    id: 7,
+    message: "GAS EVENT",
+    description: "Global Air System needs servicing",
+    messageType: "caution"
+  },
+  {
+    id: 8,
+    message: "GAS DEGRADED",
+    description: "Global Air System failure.  The system should be serviced.",
+    messageType: "caution"
+  },
+  {
+    id: 9,
+    message: "GPU DOOR",
+    description: "GPU receptacle door not closed",
+    messageType: "caution"
+  },
+  {
+    id: 10,
+    message: "GWX FAIL",
+    description:
+      "The GDU is not receiving status packet from the GWX 68/GWX 70 is reporting a fault. The GWX 68/GWX 70 radar system should be serviced",
+    messageType: "caution"
+  },
+  {
+    id: 11,
+    message: "IGNITION",
+    description: "Ignition exciter running",
+    messageType: "caution"
+  },
+  {
+    id: 12,
+    message: "INERT SEP FAIL",
+    description: "Inertial separator failure",
+    messageType: "caution"
+  },
+  {
+    id: 13,
+    message: "LOW LVL FAIL L-R*",
+    description: "Low fuel level sensor failure for specified tank",
+    messageType: "caution"
+  },
+  {
+    id: 14,
+    message: "LOW VOLTAGE",
+    description: "Battery voltage below 26 V",
+    messageType: "caution"
+  },
+  {
+    id: 15,
+    message: "MAIN GEN",
+    description: "Starter generator unconnected",
+    messageType: "caution"
+  },
+  {
+    id: 16,
+    message: "NG HI",
+    description: "Generator speed is more than 103%",
+    messageType: "caution"
+  },
+  {
+    id: 17,
+    message: "OIL PRESS",
+    description: "Oil pressure between 60 and 100 psi",
+    messageType: "caution"
+  },
+  {
+    id: 18,
+    message: "OIL TEMP",
+    description: "Oil temperature below 0�C or above 104�c",
+    messageType: "caution"
+  },
+  {
+    id: 19,
+    message: "PITOT HT ON L-R*",
+    description: "Specified pitot heat (left or right) on while engine off",
+    messageType: "caution"
+  },
+  {
+    id: 20,
+    message: "PITOT NO HT L R*",
+    description: "Specified pitot heat (left or right) off",
+    messageType: "caution"
+  },
+  {
+    id: 21,
+    message: "PRfSSU BACKUP",
+    description:
+      "GASC cannot compute optimal cabin altitude (cabin altitude default value is set at 9800 feet)",
+    messageType: "caution"
+  },
+  {
+    id: 22,
+    message: "PROP DEICE FAIL",
+    description: "Prop deice selected and not on",
+    messageType: "caution"
+  },
+  {
+    id: 23,
+    message: "PROP DEICE O\\J",
+    description: "Prop deice on while engine off",
+    messageType: "caution"
+  },
+  {
+    id: 24,
+    message: "REAR CARGO DOOR",
+    description: "The rear cargo door is open",
+    messageType: "caution"
+  },
+  {
+    id: 25,
+    message: "STALL HEAT ON",
+    description: "Stall heat on while engine off",
+    messageType: "caution"
+  },
+  {
+    id: 26,
+    message: "STALL NO HEAT",
+    description: "Stall warning heat off",
+    messageType: "caution"
+  },
+  {
+    id: 27,
+    message: "TCAS FAIL",
+    description:
+      "TCAS has failed and is inoperative.  The system should be serviced",
+    messageType: "caution"
+  },
+  {
+    id: 28,
+    message: "TRAFFIC FAIL",
+    description:
+      "Traffic has failed and is inoperative.  The system should be serviced",
+    messageType: "caution"
+  },
+  {
+    id: 29,
+    message: "VACUUM LOW",
+    description: "Vacuum pressure less than 3.75 in Hg",
+    messageType: "caution"
+  },
+  {
+    id: 30,
+    message: "USE OXYGEN MASK",
+    description: "Cabin altitude high",
+    messageType: "caution"
+  },
+  {
+    id: 31,
+    message: "XPDR1 ADS- B FAIL",
+    description:
+      "ADS-B of XPDR1 is inoperative. The transponder may not be receiving a valid GPS position. Other transponder functions may be available. Service when possible.",
+    messageType: "caution"
+  },
+  {
+    id: 32,
+    message: "XPDR1 FAIL",
+    description:
+      "XPDR1 has failed and is inoperative. The system should be serviced",
+    messageType: "caution"
+  },
+  {
+    id: 33,
+    message: "XPDR2 ADS-B FAIL",
+    description:
+      "ADS-B of XPDR2 is inoperative. The transponder may not be receiving a valid GPS position. Other transponder functions may be available. Service when possible.",
+    messageType: "caution"
+  },
+  {
+    id: 34,
+    message: "XPDR2 FAIL",
+    description:
+      "XPDR2 has failed and is inoperative. The system should be serviced",
+    messageType: "caution"
+  }
+];
---
+++ dist/warningMessages.js
@@ -0,0 +1,118 @@
+export const warningMessages = [
+  {
+    id: 0,
+    message: "ABORT APR",
+    description: "Loss of GPS navigation. Abort approach",
+    messageType: "warning"
+  },
+  {
+    id: 1,
+    message: "AURAL WRN FAIL",
+    description: "Aural warning system failure due to non-communicating LRUs",
+    messageType: "warning"
+  },
+  {
+    id: 2,
+    message: "AURAL WARN 1 CHNL",
+    description: "Aural warning failure of one LRU",
+    messageType: "warning"
+  },
+  {
+    id: 3,
+    message: "CABIN ALTITUDE",
+    description: "Cabin altitude over 10,000 ft",
+    messageType: "warning"
+  },
+  {
+    id: 4,
+    message: "CABIN DIFF PRESS",
+    description: "Cabin pressure differential over 6.2 psi",
+    messageType: "warning"
+  },
+  {
+    id: 5,
+    message: "DOOR",
+    description: "Pilot or cabin door open",
+    messageType: "warning"
+  },
+  {
+    id: 6,
+    message: "ELEC FEATH FAULT",
+    description: "Feathering system failure",
+    messageType: "warning"
+  },
+  {
+    id: 7,
+    message: "EDM",
+    description: "Autopilot has activated emergency descent mode",
+    messageType: "warning"
+  },
+  {
+    id: 8,
+    message: "FIRE",
+    description:
+      "Engine compartment fire (temperature over 200�( ; if installed)",
+    messageType: "warning"
+  },
+  {
+    id: 9,
+    message: "FLAPSASYM",
+    description: "Dissymmetry between left- and right-hand flaps",
+    messageType: "warning"
+  },
+  {
+    id: 10,
+    message: "FUEL OFF",
+    description: 'Fuel tank selectors set to "Off"',
+    messageType: "warning"
+  },
+  {
+    id: 11,
+    message: "FUEL PRESS",
+    description: "Fuel pressure below 1O psi",
+    messageType: "warning"
+  },
+  {
+    id: 12,
+    message: "GEAR UNSAFE",
+    description: "Gears are in an unsafe configuration",
+    messageType: "warning"
+  },
+  {
+    id: 13,
+    message: "ITT",
+    description:
+      "Engine start: ITT over 1000�C, 870�C (5 s), or 840�C (20 s) Engine running: ITT over 840�(",
+    messageType: "warning"
+  },
+  {
+    id: 14,
+    message: "OIL PRESS",
+    description: "Oil pressure below 60 psi",
+    messageType: "warning"
+  },
+  {
+    id: 15,
+    message: "02 CYL CLOSED",
+    description: "Oxygen cylinder closed",
+    messageType: "warning"
+  },
+  {
+    id: 16,
+    message: "PARK BRAKE",
+    description: "Parking brake applied",
+    messageType: "warning"
+  },
+  {
+    id: 17,
+    message: "PRESSU OFF",
+    description: "No Bleed air provided to cabin",
+    messageType: "warning"
+  },
+  {
+    id: 18,
+    message: "USP ACTIVE",
+    description: "Underspeed protection is active",
+    messageType: "warning"
+  }
+];
